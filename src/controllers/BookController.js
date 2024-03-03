212f29db8abe6b7711bde00b5d8c5211808d6ea2
Thercístenes <thercistenes1@outlook.com>
2024-03-03 09:57:10 -0300

    cria controler getById

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -2,9 +2,19 @@ const BookService = require('../services/BookService');
 
 const getAll = async(req, res) => {
   const books = await BookService.getAll();
-  res.status(200).json({reponse: 'ola mundo'});
+  res.status(200).json({books});
 }
+
+const getById = async(req, res) => {
+  const { id } = req.params;
+  const book = await BookService.getById(id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(book);
+};
 
 module.exports = {
   getAll,
+  getById,
 };
