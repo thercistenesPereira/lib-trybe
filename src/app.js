64318cc266cbcc21bc186e9698588cff6308c718
Thercístenes <thercistenes1@outlook.com>
2024-03-03 10:04:16 -0300

    cria rota POST /books

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ app.use(express.json());
 
 app.get('/books', BookController.getAll);
 
+app.get('/books/:id', BookController.getById);
+
+app.post('/books', BookController.create);
+
 module.exports = app;
--- src/services/BookService.js
+++ src/services/BookService.js
@@ -9,7 +9,13 @@ const getById = async (id) => {
   const book = await Book.findByPk(id);
   return book;
 };
+
+const create = async ({ title, author, pageQuantity }) => {
+  const newBook = await Book.create({ title, author, pageQuantity });
+  return newBook;
+};
 module.exports = {
   getAll,
   getById,
+  create,
 }
